5afe78871b9de95e5ef56a938bde9dbebe061ee5
Bruno Grou <bruno.grou@hotmail.com>
2023-08-31 02:21:04 +0100

    feat: Imported new packages, DatePicker, Toast Notifications and Mapbox

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,11 +4,14 @@ import '../css/app.css';
 import { createRoot } from 'react-dom/client';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import DatePicker from "react-datepicker";
+import { ToastContainer, toast } from 'react-toastify';
+import ReactMapGl from "react-map-gl";
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'WindFlow';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => `${title} - WindFlow`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
         const root = createRoot(el);
