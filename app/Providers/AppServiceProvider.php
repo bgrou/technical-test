ff9b03e7fe1b51671dfca053d0b59fb206845aaa
Bruno Grou <bruno.grou@hotmail.com>
2023-08-31 02:25:46 +0100

    feat: Shared the API so it can be accessed from Frontend

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Inertia::share('mapbox_api_token', config('app.react_app_mapbox_token'));
     }
 
     /**
