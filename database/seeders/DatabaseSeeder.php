7727557ad4888bdd2f47d5353d26cf8e6e677671
Bruno Grou <bruno.grou@hotmail.com>
2023-08-31 02:46:42 +0100

    feat: Applied the created seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            ManufacturerSeeder::class,
+            FarmSeeder::class,
+            TurbineSeeder::class,
+            ComponentSeeder::class,
+            InspectionSeeder::class
+        ]);
     }
 }
